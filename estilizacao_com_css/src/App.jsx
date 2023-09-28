f7fece8a24689327bac992cb72214ce08ab8c942
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 12:08:22 -0300

    class 55 - exercise 20: styles with external css solution

--- estilizacao_com_css/src/App.jsx
+++ estilizacao_com_css/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Button from './components/ButtonComponent/Button';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
       <div>
         <h2>1 - Hello World com estilos</h2>
         <HelloWorld />
+      </div>
+
+      <div>
+        <h2>2 - Estilo com CSS externo</h2>
+        <Button />
       </div>
     </>
   );
---
+++ estilizacao_com_css/src/components/ButtonComponent/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css';
+
+const Button = () => {
+    return (
+        <button className="my_button">Clique aqui...</button>
+    );
+};
+
+export default Button;
