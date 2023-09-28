3228f40942780de4f1cfbe38a3b5d847cb7916c6
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 11:56:08 -0300

    class 53 - exercise 19: hello world with styles solution

---
+++ estilizacao_com_css/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>1 - Hello World com estilos</h2>
+        <HelloWorld />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ estilizacao_com_css/src/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+const HelloWorld = () => {
+    const style = {
+        color: '#FDFDFD',
+        fontSize: '30px',
+        backgroundColor: '#000000',
+        borderRadius: '5px',
+        padding: '15px',
+    };
+
+    return (
+        <p style={style}>Hello World</p>
+    );
+};
+
+export default HelloWorld;
